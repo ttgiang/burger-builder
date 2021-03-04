83e486d7ea48bc8d7150c4b765a29b537cc7f233
ttgiang <ttgiang@gmail.com>
2021-03-04 10:08:15 -1000

    Firebase

---
+++ src/store/firebase/firebase.prod.js
@@ -0,0 +1,9 @@
+export const FIREBASE = "";
+
+export const API_KEY = "";
+
+export const SIGN_UP =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
+export const SIGN_IN =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
